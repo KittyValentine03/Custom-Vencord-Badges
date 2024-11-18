96ebd7e7a9ea6250485a30c29e2c961f12802a8e
Katelyn Valentine <168905070+KittyValentine03@users.noreply.github.com>
2024-11-17 23:08:58 -0800

    screm

---
+++ utils/constants.ts
@@ -0,0 +1,16 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+import { Logger } from "@utils/Logger";
+
+export const PluginLogger = new Logger("PersonalBadges", "#CD7F32");
+
+export const GITHUB_URL = "https://github.com/KittyValentine03/Custom-Vencord-Badges";
+
+export const DEFAULT_BADGE_URL = "https://i.imgur.com/3CQhzM6.png";
+export const DEFAULT_BADGE_CATEGORY = "Unassigned";
+export const DEFAULT_BADGE_CATEGORY_URL = "https://i.imgur.com/8D4hCSK.png"
+
